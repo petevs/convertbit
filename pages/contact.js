c2c62be3c294b3018826179de9519a948986295a
Pete VS <pete_vs88@me.com>
2022-03-03 12:05:30 -0700

    added contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,22 @@
 import BackgroundWapper from "../components/BackgroundWrapper"
+import { Text, Box } from "@mantine/core"
+import BackButton from "../components/BackButton"
 
 const contact = () => {
 
     return (
         <BackgroundWapper>
-            Contact
+            <Box sx={{'& a': {color: 'inherit'}}}>
+                <Text size='xl' weight={700}>
+                    Contact
+                </Text>
+                <Text
+                    mt='sm'
+                >
+                    This extension was built by <a href='https://petevs.com'>PeteVS.com</a>, any questions or concerns please reach out to <a href='mailto:hey@petevs.com?subject=Inquiry from the ConvertBit'>hey@petevs.com</a>
+                </Text>
+            </Box>
+            <BackButton />
         </BackgroundWapper>
     )
 }
